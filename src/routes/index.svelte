cfe20b3fdccdbd7677ec9e7a0c10a56cc5d5c570
Bauke van der Laan <bauke@baukevanderlaan.eu>
2022-01-10 15:14:14 +0100

    Add reproduction

---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<p>$page.url.origin: <b>{$page.url.origin}</b></p>
+<a href="/other">Navigate to other page</a>
---
+++ src/routes/other.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<p>$page.url.origin: <b>{$page.url.origin}</b></p>
+<a href="/">Navigate to initial page</a>
